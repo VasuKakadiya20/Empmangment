fd16bed48097be2dc974c2a311d3558d2fb873b5
VasuKakadiya20 <vasukakadiya1234@gmail.com>
2025-08-18 14:45:29 +0530

    employee and admin

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 
 app.get('/', (req, res) => {
